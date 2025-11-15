70cf9daf2e38ca878e41fdb8ca08c1d2cdf3b042
Seamus <harteseamus@gmail.com>
2025-11-15 16:56:28 -0500

    code.py changes 1

---
+++ code.py
@@ -0,0 +1,20 @@
+import time
+import board
+import digitalio
+import pwmio
+from adafruit_motor import servo
+from adafruit_debouncer import Debouncer
+from adafruit_hid.keyboard import Keyboard
+from adafruit_hid.keycode import Keycode
+from adafruit_hid.consumer_control import ConsumerControl
+from adafruit_hid.consumer_control_code import ConsumerControlCode
+from usb_hid import devices
+import neopixel
+import random
+import math
+import array
+import audiocore
+import audiopwmio
+import audiomixer
+
+
