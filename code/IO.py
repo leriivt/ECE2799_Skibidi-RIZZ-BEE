aa11c0c798632baecdc8e52764ec74a61777c4df
Verit Li <verit.li@gmail.com>
2025-11-18 18:17:22 -0500

    added debouncer import

--- code/IO.py
+++ code/IO.py
@@ -1,13 +1,16 @@
 import digitalio
 import board
+from adafruit_debouncer import Debouncer
 
 class IOController:
     
     def __init__(self):
         #Initialize buttons, switches, leds, amplifier, microphone
-        self.BTN_LED = digitalio.DigitalInOut(board.GP10)
-        self.BTN_LED.direction = digitalio.Direction.INPUT
-        self.BTN_LED.pull = digitalio.Pull.UP
+        self.BTN_LED_pin = digitalio.DigitalInOut(board.GP10)
+        self.BTN_LED_pin.direction = digitalio.Direction.INPUT
+        self.BTN_LED_pin.pull = digitalio.Pull.UP
+        self.BTN_LED = Debouncer(BTN_LED_pin)
+        
         self.BTN_record = digitalio.DigitalInOut(board.GP11)
         self.BTN_record.direction = digitalio.Direction.INPUT
         self.BTN_record.pull = digitalio.Pull.UP
@@ -15,7 +18,13 @@ class IOController:
         self.BTN3_audio_sel.direction = digitalio.Direction.INPUT
         self.BTN3_audio_sel.pull = digitalio.Pull.UP
         
-
+    def update_buttons(self):
+        self.BTN_LED.update()
+        pass
+        
+    def check_LED_button(self):
+        return self.BTN_LED.update.value
+    
     def read_buttons(self): #active low buttons
     
         if(not self.BTN_LED.value):
