4ed70a4ea14bf5ffc0d7692e5a95b12d232860c6
Verit Li <verit.li@gmail.com>
2025-11-18 17:40:17 -0500

    renamed BTNS to specify job

--- code/IO.py
+++ code/IO.py
@@ -5,24 +5,24 @@ class IOController:
     
     def __init__(self):
         #Initialize buttons, switches, leds, amplifier, microphone
-        self.BTN1 = digitalio.DigitalInOut(board.D2)
-        self.BTN1.direction = digitalio.Direction.INPUT
-        self.BTN1.pull = digitalio.Pull.UP
-        self.BTN2 = digitalio.DigitalInOut(board.D3)
-        self.BTN2.direction = digitalio.Direction.INPUT
-        self.BTN2.pull = digitalio.Pull.UP
-        self.BTN3 = digitalio.DigitalInOut(board.D4)
-        self.BTN3.direction = digitalio.Direction.INPUT
-        self.BTN3.pull = digitalio.Pull.UP
+        self.BTN_LED = digitalio.DigitalInOut(board.GP10)
+        self.BTN_LED.direction = digitalio.Direction.INPUT
+        self.BTN_LED.pull = digitalio.Pull.UP
+        self.BTN_record = digitalio.DigitalInOut(board.GP11)
+        self.BTN_record.direction = digitalio.Direction.INPUT
+        self.BTN_record.pull = digitalio.Pull.UP
+        self.BTN_audio_sel = digitalio.DigitalInOut(board.GP12)
+        self.BTN3_audio_sel.direction = digitalio.Direction.INPUT
+        self.BTN3_audio_sel.pull = digitalio.Pull.UP
         
 
     def read_buttons(self): #active low buttons
     
-        if(not self.BTN1.value):
+        if(not self.BTN_LED.value):
             return 1
-        elif(not self.BTN2.value):
+        elif(not self.BTN_record.value):
             return 2
-        elif(not self.BTN3.value):
+        elif(not self.BTN_audio_sel.value):
             return 3
         else: #No button pressed
             return 0
