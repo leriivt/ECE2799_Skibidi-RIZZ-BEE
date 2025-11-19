b3f9428a3765ad573b20fc7b1c8d4177e52f607c
leriivt <90656798+leriivt@users.noreply.github.com>
2025-11-18 19:50:48 -0500

    after testing 11/18, switching btwn leds works

--- code/LEDs.py
+++ code/LEDs.py
@@ -21,7 +21,14 @@ NUM_STATICS = 8
 
 colors = {
     OFF : (0 ,0, 0),
-    RED : (255, 0, 0)
+    RED : (255, 0, 0),
+    ORANGE : (255, 128, 0),
+    YELLOW : (255, 255, 51),
+    GREEN : (51, 255, 51),
+    BLUE : (0, 128, 255),
+    PURPLE : (153, 51, 255),
+    PINK : (255, 51, 153),
+    RAINBOW : (128, 128, 128)
 }
 
 class LEDController:
@@ -29,9 +36,9 @@ class LEDController:
     def __init__(self, pattern=RED, static=1):
         self.pattern = pattern
         self.static = static
-        self.num_pixels = 12
+        self.num_pixels = 3
         
-        self.pixels = neopixel.NeoPixel(board.GP22, num_pixels)
+        self.pixels = neopixel.NeoPixel(board.GP0, self.num_pixels) #note was GP22
         self.pixels.brightness = 0.5
         #Initialize LED components
         pass
@@ -39,15 +46,16 @@ class LEDController:
     def increment_pattern(self):        
         self.pattern += 1
         if self.pattern >= NUM_PATTERNS:
-            self.patern = 0
+            self.pattern = 0
             self.static = 1
-        else if self.static && self.pattern >= NUM_STATICS:
+        elif self.static and self.pattern >= NUM_STATICS:
             self.static = 0
         pass
         
     def update_pattern(self):
         if self.static:
             self.pixels.fill(colors[self.pattern])
-        else
+        else:
             pass
         pass
+
