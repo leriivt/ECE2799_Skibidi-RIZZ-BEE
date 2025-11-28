4d1db934f046af739bc93e8d6a8b676b1aeb42cc
Verit Li <verit.li@gmail.com>
2025-11-28 17:30:59 -0500

    now use Button class instead of Debouncer, press and hold for recording implemented

--- code/IO.py
+++ code/IO.py
@@ -1,6 +1,7 @@
 import digitalio
 import board
 from adafruit_debouncer import Debouncer
+from adafruit_debouncer import Button
 
 class IOController:
 
@@ -8,38 +9,50 @@ class IOController:
     
     def __init__(self):
         #Initialize buttons 
-        self.BTN_LED_pin = digitalio.DigitalInOut(board.GP2) #was 10
+        self.BTN_record_pin = digitalio.DigitalInOut(board.GP0)
+        self.BTN_record_pin.direction = digitalio.Direction.INPUT
+        self.BTN_record_pin.pull = digitalio.Pull.UP
+        self.BTN_record = Button(self.BTN_record_pin)
+        
+        self.BTN_audio_sel_pin = digitalio.DigitalInOut(board.GP1)
+        self.BTN_audio_sel_pin.direction = digitalio.Direction.INPUT
+        self.BTN_audio_sel_pin.pull = digitalio.Pull.UP
+        self.BTN_audio_sel = Button(self.BTN_audio_sel_pin)
+        
+        self.BTN_LED_pin = digitalio.DigitalInOut(board.GP3)
         self.BTN_LED_pin.direction = digitalio.Direction.INPUT
         self.BTN_LED_pin.pull = digitalio.Pull.UP
-        self.BTN_LED = Debouncer(self.BTN_LED_pin)
-        '''
-        self.BTN_record = digitalio.DigitalInOut(board.GP11)
-        self.BTN_record.direction = digitalio.Direction.INPUT
-        self.BTN_record.pull = digitalio.Pull.UP
-        self.BTN_audio_sel = digitalio.DigitalInOut(board.GP12)
-        self.BTN3_audio_sel.direction = digitalio.Direction.INPUT
-        self.BTN3_audio_sel.pull = digitalio.Pull.UP
-        '''
+        self.BTN_LED = Button(self.BTN_LED_pin)
 
         
     def update_buttons(self):
+        self.BTN_record.update()
+        self.BTN_audio_sel.update()
         self.BTN_LED.update()
         pass
-        
+    
+    '''    
     def check_LED_button(self):
         #1 if not pressed
         #we want 1 if pressed, so add a not
-        return self.BTN_LED.fell
+        return self.BTN_LED.pressed
+    '''
     
     def read_buttons(self): #active low buttons
     
-        if(not self.BTN_LED.value):
+        if(self.BTN_record.long_press):
             return 1
-        elif(not self.BTN_record.value):
+        elif(self.BTN_audio_sel.pressed):
             return 2
-        elif(not self.BTN_audio_sel.value):
+        elif(self.BTN_LED.pressed):
             return 3
         else: #No button pressed
             return 0
         
+    def check_recording(self):
+        return not self.BTN_record.released
+        
+
+
+
 
