05bf9a1320a67ec36714eba95e497e22ff4a7388
Verit Li <verit.li@gmail.com>
2025-11-28 23:35:15 -0500

    added rainbow, HUNTRIX, and golden modes

--- code/LEDs.py
+++ code/LEDs.py
@@ -1,6 +1,7 @@
 import board
 import neopixel
 import time
+from rainbowio import colorwheel
 
 '''
     Need code for:
@@ -8,7 +9,7 @@ import time
             - store which pattern
         - 
 '''
-NUM_PIXELS = 1
+NUM_PIXELS = 12
 
 OFF = 0
 RED = 1
@@ -19,7 +20,9 @@ BLUE = 3
 PURPLE = 4
 #PINK = 7
 RAINBOW = 5
-NUM_PATTERNS = 6
+HUNTRIX = 6
+GOLDEN = 7
+NUM_PATTERNS = 8
 NUM_STATICS = 5
 
 DIM = 100
@@ -34,15 +37,43 @@ colors = {
     BLUE : [(0, 128, 255)] * NUM_PIXELS,
     PURPLE : [(153, 51, 255)] * NUM_PIXELS,
     #PINK : (255, 51, 153),
-    RAINBOW : [(255, 255, 51)] * NUM_PIXELS,
+    RAINBOW : [(255, 255, 255)] * NUM_PIXELS, #placeholder
+    HUNTRIX : [(255, 255, 255)] * NUM_PIXELS, #placeholder
+    GOLDEN : [(255, 255, 255)] * NUM_PIXELS,  #placeholder
     DIM : [(20 ,20, 20)] * NUM_PIXELS
 }
+
+def hue_cycle(current, start, end, multiplier, num_pixels):
+    hue_length = (end-start)*2
+    current = current % hue_length
+    pixel_list = [0] * num_pixels
+    for pixel in range(num_pixels):
+        pixel_index = (pixel * (hue_length+1) // num_pixels) + current * multiplier
+        pixel_index = pixel_index % hue_length
+        pixel_index += start
+        if pixel_index > end:
+            pixel_index = 2*end - pixel_index
+        pixel_list[pixel] = colorwheel(pixel_index)
+    return pixel_list
+
+def rainbow_cycle(current, num_pixels):
+    color = current % 255
+    pixel_list = [0] * num_pixels
+    for pixel in range(num_pixels):
+        pixel_index = (pixel * 256 // num_pixels) + color * 5
+        pixel_list[pixel] = colorwheel(pixel_index & 255)
+    return pixel_list
+
+colors[RAINBOW] = rainbow_cycle(0, NUM_PIXELS)
+colors[HUNTRIX] = hue_cycle(0, 110, 230, 5, NUM_PIXELS)
+colors[GOLDEN] = hue_cycle(0, 14, 22, 4, NUM_PIXELS)
 
 class LEDController:
     
     def __init__(self, pattern=RED, static=1, brightness=0.5):
         self.pattern = pattern
         self.static = static
+        self.dynamic_offset = 0 #offset for cycling through dynamic patterns
         self.num_pixels = NUM_PIXELS
         
         self.pixels = neopixel.NeoPixel(board.GP22, self.num_pixels)
@@ -102,7 +133,52 @@ class LEDController:
         self.show_pattern()
         self.update_pattern(OFF)
         pass
-
-
+    
+    def rainbow_cycle(self, wait):
+        for color in range(255):
+            for pixel in range(len(self.pixels)):
+                pixel_index = (pixel * 256 // len(self.pixels)) + color * 5
+                self.pixels[pixel] = colorwheel(pixel_index & 255)
+            self.pixels.show()
+            time.sleep(wait)
+            
+    def galaxy_cycle(self, wait): #colorwheel vales 110 to 230
+        for color in range(240):
+            for pixel in range(len(self.pixels)):
+                pixel_index = (pixel * 241 // len(self.pixels)) + color * 5
+                pixel_index = pixel_index % 240
+                pixel_index += 110
+                if pixel_index > 230:
+                    pixel_index = 2*230 - pixel_index
+                self.pixels[pixel] = colorwheel(pixel_index)
+            self.pixels.show()
+            time.sleep(wait)
+            
+    def hue_cycle_testing(self, start, end, multiplier, wait):
+        length = (end-start)*2
+        for color in range(length):
+            for pixel in range(len(self.pixels)):
+                pixel_index = (pixel * (length+1) // len(self.pixels)) + color * multiplier
+                pixel_index = pixel_index % length
+                pixel_index += start
+                if pixel_index > end:
+                    pixel_index = 2*end - pixel_index
+                self.pixels[pixel] = colorwheel(pixel_index)
+            self.pixels.show()
+            time.sleep(wait)
+    '''        
+    def hue_cycle(self, current, start, end, multiplier, wait):
+        length = (end-start)*2
+        current = current % length
+            for pixel in range(len(self.pixels)):
+                pixel_index = (pixel * (length+1) // len(self.pixels)) + color * multiplier
+                pixel_index = pixel_index % length
+                pixel_index += start
+                if pixel_index > end:
+                    pixel_index = 2*end - pixel_index
+                self.pixels[pixel] = colorwheel(pixel_index)
+            self.pixels.show()
+            time.sleep(wait)
+    '''
 
 
